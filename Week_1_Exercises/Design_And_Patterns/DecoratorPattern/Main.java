df37728a0166089d4023e6b3ca7eea5402488ef2
Nitheswari.D <103818470+Nitheswari-D@users.noreply.github.com>
2024-08-01 00:31:05 +0530

    Add files via upload

---
+++ Week_1_Exercises/Design_And_Patterns/DecoratorPattern/Main.java
@@ -0,0 +1,12 @@
+package decoratorpattern;
+
+public class Main {
+    public static void main(String[] args) {
+        Notifier notifier = new EmailNotifier();
+
+        notifier = new SMSNotifierDecorator(notifier);
+        notifier = new SlackNotifierDecorator(notifier);
+
+        notifier.send("Hello World!");
+    }
+}
---
+++ Week_1_Exercises/Design_And_Patterns/DecoratorPattern/SlackNotifierDecorator.java
@@ -0,0 +1,13 @@
+package decoratorpattern;
+
+public class SlackNotifierDecorator extends NotifierDecorator {
+    public SlackNotifierDecorator(Notifier wrapped) {
+        super(wrapped);
+    }
+
+    @Override
+    public void send(String message) {
+        super.send(message);
+        System.out.println("Sending Slack message: " + message);
+    }
+}
