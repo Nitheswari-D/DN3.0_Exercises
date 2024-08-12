b0469c830948fef334a2fccea6b494ca74fe6c97
Nitheswari.D <103818470+Nitheswari-D@users.noreply.github.com>
2024-08-12 18:23:53 +0530

    Add files via upload

---
+++ Week_2_Exercises/Spring and Maven/Exercise_2/BookRepository.java
@@ -0,0 +1,8 @@
+package com.library.repository;
+
+public class BookRepository {
+    public void saveBook(String bookName) {
+        // Simulate saving a book to the repository
+        System.out.println("Saving book: " + bookName);
+    }
+}
