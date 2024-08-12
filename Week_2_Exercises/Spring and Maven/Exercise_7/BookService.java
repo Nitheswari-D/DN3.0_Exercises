55a870898d4ce83cacbc3ea641c082d5e21fc4ca
Nitheswari.D <103818470+Nitheswari-D@users.noreply.github.com>
2024-08-12 18:27:00 +0530

    Add files via upload

---
+++ Week_2_Exercises/Spring and Maven/Exercise_7/BookService.java
@@ -0,0 +1,23 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+
+public class BookService {
+
+    private BookRepository bookRepository;
+
+    // Constructor-based injection
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    // Setter-based injection
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void performService() {
+        System.out.println("Performing service...");
+        bookRepository.performRepositoryTask();
+    }
+}
